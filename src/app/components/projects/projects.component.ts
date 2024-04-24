05029e127c397f1e54e72fcdd806aea53b6271ad
Benjamin Contreras <benja.contreras732@gmail.com>
2024-04-24 00:16:34 -0400

    Portafolio 1.0

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import projectsData from "../../data/projectsData.json"
+
+interface ProjectsData{
+  title: string
+  description: string
+  githubURL?: string
+}
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
 })
 export class ProjectsComponent {
+
+  projectsData: ProjectsData[] = projectsData; 
 
 }
